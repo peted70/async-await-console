6b5a797b048a32b55fb839921f3ed394f02f5a37
peted <peted70@hotmail.com>
2015-02-18 16:19:46 +0000

    initial commit

---
+++ AsyncAwait/Api.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    static internal class Api
+    {
+        internal const string key = "xxxxxxxxxxxxxxxxxxxx";
+    }
+}
